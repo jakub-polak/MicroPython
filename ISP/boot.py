acb6c55d96620fd9e1524d34c0a9852e18e9b551
polak95 <jakubpolak95@gmail.com>
2018-12-22 22:03:06 +0100

    update boot.py

--- ISP/boot.py
+++ ISP/boot.py
@@ -4,7 +4,6 @@ import gc
 import webrepl
 import isp
 import machine
-from machine import Pin
 import time
 
 webrepl.start()
@@ -14,7 +13,7 @@ gc.collect()
 pin_temp = machine.Pin(2)  # D4
 pin_light = machine.ADC(0)  # A0
 
-mig_mig = Pin(12, Pin.OUT) #D6
+mig_mig = machine.Pin(12, machine.Pin.OUT) #D6
 mig_mig.on()
 
 # CONNECTION
@@ -27,43 +26,9 @@ mig_mig.off()
 # OneWire
 ds, x = isp.connect_onewire(pin_temp)
 
-for i in range(2):
-    mig_mig.value(1)
-    time.sleep_ms(100)
-    mig_mig.value(0)
-    time.sleep_ms(100)
-    mig_mig.value(1)
-    time.sleep_ms(100)
-    mig_mig.value(0)
-    time.sleep_ms(100)
-    mig_mig.value(1)
-    time.sleep_ms(100)
-    mig_mig.value(0)
-    time.sleep_ms(2000)
-
 # MQTT
 c = isp.connect_MQTT()
-
-for i in range(2):
-    mig_mig.value(1)
-    time.sleep_ms(100)
-    mig_mig.value(0)
-    time.sleep_ms(100)
-    mig_mig.value(1)
-    time.sleep_ms(100)
-    mig_mig.value(0)
-    time.sleep_ms(100)
-    mig_mig.value(1)
-    time.sleep_ms(100)
-    mig_mig.value(0)
-    time.sleep_ms(2000)
 
 while True:
     c.publish(b"micropython/2", isp.get_temp(ds, x))
     time.sleep_ms(1000)
-
-while True:
-    mig_mig.value(1)
-    time.sleep_ms(100)
-    mig_mig.value(0)
-    time.sleep_ms(100)
